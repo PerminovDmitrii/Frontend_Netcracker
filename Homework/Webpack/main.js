ce3194a51342721a84489f66c675d73d3657cb94
PerminovDmitrii <sqrt008@gmail.com>
2021-04-09 15:59:56 +0300

    fix

--- Homework/Webpack/main.js
+++ Homework/Webpack/main.js
@@ -2,8 +2,6 @@ import {stack} from './src/resourcesStack';
 import {pushElement} from './src/pushElement';
 import {getElement} from './src/getElemernt';
 
-const pushButton = document.getElementById("push-button");
-const getButton = document.getElementById('get-button');
-
-pushButton.addEventListener('onclick', pushElement(stack));
-getButton.addEventListener('onclick', getElement(stack));
+window.stack = stack;
+window.pushElement = pushElement;
+window.getElement = getElement;
